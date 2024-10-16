be259ea323eb9370556009e63d04e8e773d153f2
Tyler Brown <browntylerr1@gmail.com>
2024-10-16 16:19:44 -0500

    updated
    added constructor with getters and setters

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,5 +1,65 @@
 package com.pluralsight;
 import java.time.LocalDate;
 public class Transaction {
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
 
+    // constructor
+    public Transaction(LocalDate _date, LocalTime _time, String _description, String _vendor, double _amount) {
+        this.date = _date;
+        this.time = _time;
+        this.description = _description;
+        this.vendor = _vendor;
+        this.amount = _amount;
+    }
+    // getters and setters
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+
+
+    // Updated toString method to match the desired output format
+    @Override
+    public String toString() {
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return String.format("%s|%s|%s|%s|%.2f", date, time.format(timeFormatter), description, vendor, amount);
+    }
 }
